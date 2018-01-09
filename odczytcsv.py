f7b450342d6bbe35a184cb96d0dceda79b0f323d
ela1357 <lubanska.ela@gmail.com>
2018-01-09 14:16:46 +0100

    Struktury.py

---
+++ odczytcsv.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  odczytcsv.py
+#  
+
+import csv
+
+def main(args):
+    plik = open("samochody.txt")
+    tekst = plik.read()
+    plik.close()
+    print(tekst)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
