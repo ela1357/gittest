9221f671b43c2f2bcfabce2a764c8da3a81ce321
ela1357 <lubanska.ela@gmail.com>
2018-02-01 13:30:09 +0100

    Lista

---
+++ cpp_klasa3/Struktury/lista.hpp
@@ -0,0 +1,21 @@
+#ifndef LISTA_HPP
+#define LISTA_HPP
+
+struct ELEMENT{ // rekurencyjny sposób deklarowania listy
+    int wartosc;
+    ELEMENT *nast; // wskaźnik do nastepnego elementu listy
+};
+
+class Lista {
+    private: // hermetyzacja danych
+        ELEMENT *head;
+        ELEMENT *tail;
+    public: // interfejs publiczny (API klasy)
+        Lista(); // konstruktor
+        ~Lista (); // destruktor, sprząta po klasie // memory leaks - wycieki pamięci
+        void Dodaj(int);
+        void Wyswietl();
+        bool Usun();        
+};
+
+#endif
