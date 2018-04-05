dacc94eec3ab94dd10dbc2baee8cd72b2742a468
ela1357 <lubanska.ela@gmail.com>
2018-04-05 13:28:53 +0200

    p5js - ruchy browna

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -11,29 +11,56 @@
 
 //     pi = 4 * ileKo / ileKw
 //     print("Przybliżona wartość pi: ", pi)
+
+szerokosc = 640;
+wysokosc = 480;
+
 ileKw = prompt("Podaj ilość punktów: ", 100);
-ileKo = 0;
+ileKo = parseInt(ileKw);
 r = prompt("Podaj promień: ", 100);
+ileKo = 0;
 
 function getRnd(min, max) {
 	return Math.random() * (max - min) + min;
 }
 
+lx = [];
+ly = [];
+
 for (i = 0; i < ileKw; i++) {
 	x = Math.floor(getRnd(-r, r));
 	y = Math.floor(getRnd(-r, r));
 	// console.log(x);
+	lx[i] = x;
+	ly[i] = y;
 	if (x*x + y*y <= r*r) {
 		ileKo++;
 	}
 }
 
-console.log("Liczba Pi: " + (4 * ileKo / ileKw))
+// console.log(x);
+pi = 4* ileKo / ileKw;
+alert("Przybliżona wartość pi: " + pi);
 
 function setup() {
-  // put setup code here
+  createCanvas(szerokosc, wysokosc);
+  stroke('#666');
+  background('#fff');
+  ellipse(szerokosc / 2, wysokosc / 2, 2 * r, 2 * r);
+  line(szerokosc / 2, 0, szerokosc / 2, wysokosc);
+  line(0, wysokosc / 2, szerokosc, wysokosc /2);
 }
 
+x_offset = szerokosc / 2;
+y_offset = wysokosc / 2;
+
 function draw() {
-  // put drawing code here
+	for (i = 0; i < ileKw; i++) {
+		if (lx[i]*lx[i] + ly[i]*ly[i] <= r*r) {
+			stroke('#f00');
+		} else {
+			stroke('#000');
+		}
+		point(lx[i] + x_offset, ly[i] + y_offset);
+	}
 }
